b1ca767abaf61a8df0bf3424256ff86a5c24aeac
cocryv <60353356+cocryv@users.noreply.github.com>
2022-04-27 01:29:00 +0200

    add tweet button sidebar and pp create

--- components/Post.js
+++ components/Post.js
@@ -31,15 +31,15 @@ const Post = ({updateTweets}) => {
     }
 
     return (
-        <div className='flex items-center w-full'>
-            <div className='w-12 px-4'>
-                <p></p>
+        <div className='flex w-full'>
+            <div className='px-4'>
+            <img className='inline-block h-9 w-full rounded-full' src="https://pbs.twimg.com/profile_images/1308769664240160770/AfgzWVE7_normal.jpg" alt="" />
             </div>
              <form className='w-full flex flex-col '>
                 <input ref={bodyInput} className="px-3 py-4 placeholder-white text-white relative bg-twitter-dark rounded text-base border-0 outline-none focus:outline-none w-full h-8" type="text" id="tweet" name="tweet" placeholder='What is happening?'/>
                 <div className='flex justify-end pr-4 pt-4'>
                     <button onClick={handleSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-3xl">
-                        Tweeter
+                        Tweet
                     </button>
                 </div>
             </form> 
--- pages/index.js
+++ pages/index.js
@@ -31,8 +31,8 @@ export default function Home({tweets}) {
       <div className="container mx-auto min-h-screen">
         <div className='flex flex-row justify-center'>
           <div className='w-68 xs:w-88 xl:w-275 sticky h-screen border border-black'>
-            <div className='flex flex-col h-screen xl:pr-3 fixed overflow-y-auto w-68 xs:w-88 xl:w-275'>
-              <h1 className='h-14'>
+            <div className='flex flex-col h-screen xl:pr-3 overflow-y-auto w-68 xs:w-88 xl:w-275'>
+              <h1 className='h-14 flex items-center justify-center xl:justify-start'>
                 <a href="">
                 <svg viewBox="0 0 24 24" class="w-8 h-8 text-blue-400 dark:text-white" fill="#FFFF">
                   <g>
@@ -41,30 +41,30 @@ export default function Home({tweets}) {
                 </svg>
                 </a>
               </h1>
-              <nav className='flex flex-col gap-2'>
+              <nav className='flex flex-col gap-8 xl:gap-2 mt-4'>
                 <a className='flex gap-2 justify-center xl:justify-start' href="">
                   <svg fill="currentColor" viewBox="0 0 24 24" class="h-6 w-6">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M22.58 7.35L12.475 1.897c-.297-.16-.654-.16-.95 0L1.425 7.35c-.486.264-.667.87-.405 1.356.18.335.525.525.88.525.16 0 .324-.038.475-.12l.734-.396 1.59 11.25c.216 1.214 1.31 2.062 2.66 2.062h9.282c1.35 0 2.444-.848 2.662-2.088l1.588-11.225.737.398c.485.263 1.092.082 1.354-.404.263-.486.08-1.093-.404-1.355zM12 15.435c-1.795 0-3.25-1.455-3.25-3.25s1.455-3.25 3.25-3.25 3.25 1.455 3.25 3.25-1.455 3.25-3.25 3.25z"></path>
                   </svg>
-                  <span className='hidden xl:block ml-4 font-bold text-md mb-8'>Accueil</span>
+                  <span className='hidden xl:block ml-4 font-bold text-md mb-8'>Home</span>
                 </a>
                 <a className='flex gap-2 justify-center xl:justify-start' href="">
                   <svg fill="currentColor" viewBox="0 0 24 24" class="h-6 w-6">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 7.337h-3.93l.372-4.272c.036-.412-.27-.775-.682-.812-.417-.03-.776.27-.812.683l-.383 4.4h-6.32l.37-4.27c.037-.413-.27-.776-.68-.813-.42-.03-.777.27-.813.683l-.382 4.4H3.782c-.414 0-.75.337-.75.75s.336.75.75.75H7.61l-.55 6.327H3c-.414 0-.75.336-.75.75s.336.75.75.75h3.93l-.372 4.272c-.036.412.27.775.682.812l.066.003c.385 0 .712-.295.746-.686l.383-4.4h6.32l-.37 4.27c-.036.413.27.776.682.813l.066.003c.385 0 .712-.295.746-.686l.382-4.4h3.957c.413 0 .75-.337.75-.75s-.337-.75-.75-.75H16.39l.55-6.327H21c.414 0 .75-.336.75-.75s-.336-.75-.75-.75zm-6.115 7.826h-6.32l.55-6.326h6.32l-.55 6.326z"></path>
                   </svg>
-                  <span className='hidden xl:block ml-4 font-bold text-md mb-8'>Explorer</span>
+                  <span className='hidden xl:block ml-4 font-bold text-md mb-8'>Explore</span>
                 </a>
                 <a className='flex gap-2 justify-center xl:justify-start' href="">
                   <svg fill="currentColor" viewBox="0 0 24 24" class="h-6 w-6">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21.697 16.468c-.02-.016-2.14-1.64-2.103-6.03.02-2.532-.812-4.782-2.347-6.335C15.872 2.71 14.01 1.94 12.005 1.93h-.013c-2.004.01-3.866.78-5.242 2.174-1.534 1.553-2.368 3.802-2.346 6.334.037 4.33-2.02 5.967-2.102 6.03-.26.193-.366.53-.265.838.102.308.39.515.712.515h4.92c.102 2.31 1.997 4.16 4.33 4.16s4.226-1.85 4.327-4.16h4.922c.322 0 .61-.206.71-.514.103-.307-.003-.645-.263-.838zM12 20.478c-1.505 0-2.73-1.177-2.828-2.658h5.656c-.1 1.48-1.323 2.66-2.828 2.66zM4.38 16.32c.74-1.132 1.548-3.028 1.524-5.896-.018-2.16.644-3.982 1.913-5.267C8.91 4.05 10.397 3.437 12 3.43c1.603.008 3.087.62 4.18 1.728 1.27 1.285 1.933 3.106 1.915 5.267-.024 2.868.785 4.765 1.525 5.896H4.38z"></path>
                   </svg>
-                  <span className='hidden xl:block ml-4 font-bold text-md mb-8'>Notification</span>
+                  <span className='hidden xl:block ml-4 font-bold text-md mb-8'>Notifications</span>
                 </a>
                 <a className='flex gap-2 justify-center xl:justify-start' href="">
                   <svg fill="currentColor" viewBox="0 0 24 24" class="h-6 w-6">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19.25 3.018H4.75C3.233 3.018 2 4.252 2 5.77v12.495c0 1.518 1.233 2.753 2.75 2.753h14.5c1.517 0 2.75-1.235 2.75-2.753V5.77c0-1.518-1.233-2.752-2.75-2.752zm-14.5 1.5h14.5c.69 0 1.25.56 1.25 1.25v.714l-8.05 5.367c-.273.18-.626.182-.9-.002L3.5 6.482v-.714c0-.69.56-1.25 1.25-1.25zm14.5 14.998H4.75c-.69 0-1.25-.56-1.25-1.25V8.24l7.24 4.83c.383.256.822.384 1.26.384.44 0 .877-.128 1.26-.383l7.24-4.83v10.022c0 .69-.56 1.25-1.25 1.25z"></path>
                   </svg>
-                  <span className='hidden xl:block ml-4 font-bold text-md mb-8'>Message</span>
+                  <span className='hidden xl:block ml-4 font-bold text-md mb-8'>Messages</span>
                 </a>
                 <a className='flex gap-2 justify-center xl:justify-start mb-8' href="">
                   <svg fill="currentColor" viewBox="0 0 24 24" class="h-6 w-6">
@@ -73,15 +73,20 @@ export default function Home({tweets}) {
                   <span className='hidden xl:block ml-4 font-bold text-md'>Profil</span>
                 </a>
               </nav>
-              <div>
-                Tweeter
+              <div className='flex items-center justify-center w-full pl-2'>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full xl:rounded-3xl w-full">
+                  <svg fill="currentColor" viewBox="0 0 24 24" class="block xl:hidden h-6 w-6">
+                    <path d="M8.8 7.2H5.6V3.9c0-.4-.3-.8-.8-.8s-.7.4-.7.8v3.3H.8c-.4 0-.8.3-.8.8s.3.8.8.8h3.3v3.3c0 .4.3.8.8.8s.8-.3.8-.8V8.7H9c.4 0 .8-.3.8-.8s-.5-.7-1-.7zm15-4.9v-.1h-.1c-.1 0-9.2 1.2-14.4 11.7-3.8 7.6-3.6 9.9-3.3 9.9.3.1 3.4-6.5 6.7-9.2 5.2-1.1 6.6-3.6 6.6-3.6s-1.5.2-2.1.2c-.8 0-1.4-.2-1.7-.3 1.3-1.2 2.4-1.5 3.5-1.7.9-.2 1.8-.4 3-1.2 2.2-1.6 1.9-5.5 1.8-5.7z"></path>
+                  </svg>
+                  <span className='hidden xl:block'>Tweet</span>
+                </button>
               </div>
             </div>
           </div>
           <div className='w-full sm:w-600 min-h-screen border border-black'>
             <div className="flex justify-center items-center h-14 border border-black">
               <div className='flex w-full'>
-                <h1>Home</h1>
+                <h1 className='pl-4 font-bold'>Home</h1>
               </div>
             </div>
             <div className="flex justify-center items-center h-36 border border-black">
